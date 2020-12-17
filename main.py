f094ac22c4e8425686eff2eab3305fe937e08770
Emil Burzo <contact@emilburzo.com>
2020-12-17 09:34:01 +0200

    nicer logging output

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import time
 
 import requests
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 log = logging.getLogger(__name__)
 
 STATUS_HOME = "home"
