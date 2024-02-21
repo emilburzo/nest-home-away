91474e6a52652a44dd72c9bf643f45bcc621e286
Emil Burzo <contact@emilburzo.com>
2024-02-21 18:12:15 +0200

    switch to k8s

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kubectl apply -f .ci/deploy.yaml
+SHORT_SHA="$(git rev-parse --short HEAD)"
+echo "SHORT_SHA=${SHORT_SHA}"
+cat .ci/deploy.yaml | sed "s/SHORT_SHA/${SHORT_SHA}/g" | kubectl apply -f -
